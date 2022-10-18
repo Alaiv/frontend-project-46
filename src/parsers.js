964c333310177be7d3be3deca7ac4e08e5ed6fc5
Aleks <lasacc4@yandex.ru>
2022-10-18 13:28:18 +0300

    refactoring

--- src/parsers.js
+++ src/parsers.js
@@ -2,18 +2,17 @@ import yaml from 'js-yaml';
 import { readFileSync } from 'fs';
 import path from 'path';
 
+const getFileInfo = (extName, pathName) => (extName === '.yml' || extName === '.yaml'
+  ? yaml.load(readFileSync(pathName, 'utf8'))
+  : JSON.parse(readFileSync(pathName, 'utf8')));
+
 const parser = (filepath1, filepath2) => {
   const [path1, path2] = [path.resolve(filepath1), path.resolve(filepath2)];
   const extName1 = path.extname(filepath1);
   const extName2 = path.extname(filepath2);
 
-  const file1 = extName1 === '.yml' || extName1 === '.yaml'
-    ? yaml.load(readFileSync(path1, 'utf8'))
-    : JSON.parse(readFileSync(path1, 'utf8'));
-
-  const file2 = extName2 === '.yml' || extName1 === '.yaml'
-    ? yaml.load(readFileSync(path2, 'utf8'))
-    : JSON.parse(readFileSync(path2, 'utf8'));
+  const file1 = getFileInfo(extName1, path1);
+  const file2 = getFileInfo(extName2, path2);
 
   return [file1, file2];
 };
