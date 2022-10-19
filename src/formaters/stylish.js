4d8227aeaefe90164c735fd3687af9b0bf39bb3f
Aleks <lasacc4@yandex.ru>
2022-10-19 12:52:22 +0300

    refactor formatter

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,19 +1,18 @@
 import _ from 'lodash';
 
 const stylish = (data, dex = ' ') => {
-  if (!data || !_.isObject(data)) return false;
+  if (!_.isObject(data)) return false;
   const defSpace = 2;
   const addSpaceCount = 4;
+  const signs = ['+', '-'];
 
   const iter = (dataValue, del) => {
-    if (!_.isObject(dataValue)) {
-      return `${dataValue}`;
-    }
+    if (!_.isObject(dataValue)) return `${dataValue}`;
     const finalInd = dex.repeat(del - defSpace);
 
     const mapped = Object.entries(dataValue)
       .map(([key, value]) => {
-        const reps = key.startsWith('+') || key.startsWith('-') ? del : del + defSpace;
+        const reps = signs.includes(key[0]) ? del : del + defSpace;
         const defaultInd = dex.repeat(reps);
         return `${defaultInd}${key}: ${iter(value, del + addSpaceCount)}`;
       });
